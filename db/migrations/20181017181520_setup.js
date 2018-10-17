3a1fe12a454eb94363f711d30162137b408fa917
Connor Peebles <connorpeebles93@gmail.com>
2018-10-17 18:46:49 +0000

    setup database with three tables: food, location, and a join table

---
+++ db/migrations/20181017181520_setup.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+
+    knex.schema.createTable("food", function(table) {
+      table.increments("id");
+      table.string("name");
+      table.string("description");
+      table.float("price");
+      table.string("image_url");
+    }),
+
+    knex.schema.createTable("location", function(table) {
+      table.increments("id");
+      table.string("address");
+      table.string("phone_number");
+    }),
+
+    knex.schema.createTable("food_location_join", function(table) {
+      table.integer("food_id");
+      table.integer("location_id");
+      table.foreign("food_id").references("id").inTable("food");
+      table.foreign("location_id").references("id").inTable("location");
+    })
+
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable("food"),
+    knex.schema.dropTable("location"),
+    knex.schema.dropTable("food_location_join")
+  ]);
+};
