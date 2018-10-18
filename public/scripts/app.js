ea8f3b990594f0aa0c2a7a9d8c85020129337770
= <landendanyluk@gmail.com>
2018-10-18 16:26:07 +0000

    Add db link

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,17 @@ $(() => {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+
+  $('#sign-in-button').click(function(e) {
+  	e.preventDefault();
+  	$.ajax({
+  		method: 'POST',
+  		url: '/api/users/register',
+  		data: {
+  			email: $('#email').val(),
+  			password: $('#password').val()
+  		}
+  	});
+  });
+
 });
--- server.js
+++ server.js
@@ -14,19 +14,15 @@ const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
-const cookieMonster = require('cookie-session');
-app.use(cookieMonster({
-	name: 'session',
-	keys: ['hgiehgiehiwheihge', 'eutiewginibebi']
-}));
-const bcrypt = require('bcrypt');
-
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 const foodRoutes = require("./routes/food");
 
-const userHelpers = require('./lib/user-helpers');
-
+const cookieSession = require('cookie-session');
+app.use(cookieSession({
+	name: 'session',
+	keys: ['hgiehgiehiwheihge', 'eutiewginibebi']
+}));
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -54,14 +50,6 @@ app.get("/", (req, res) => {
   res.render("index");
   console.log(req.session);
 });
-
-app.post("/login", (req, res) => {
-	const {email, password} = req.body || undefined;
-	const user_id = userHelpers.generateAlphanumericString();
-	const hashedPassword = bcrypt.hashSync(password, 10);
-	req.session.user_id = user_id;
-	res.redirect('/');
-})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
