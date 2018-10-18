6351ce9ab8187feb2cefa60a2b1a255342abb927
= <landendanyluk@gmail.com>
2018-10-18 22:39:51 +0000

    Nav link to root, remove reference to deprecated user handle

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,4 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-
   $('#sign-in-button').click(function(e) {
   	e.preventDefault();
   	$.ajax({
