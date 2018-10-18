fa6b61ed6b3fe2156fb04a61834cdf232a67c659
Connor Peebles <connorpeebles93@gmail.com>
2018-10-18 23:12:10 +0000

    fixed bug where quantites of donuts do not update in cookies

--- public/scripts/food_app.js
+++ public/scripts/food_app.js
@@ -23,51 +23,65 @@ $(() => {
               </div>
             </div>
 
-          <div class="row">
-            <div class="col-md-6">
-              <p class="price">Per donut: $${foods[i].price}</p>
+            <div class="row">
+              <div class="col-md-12">
+                <p class="price">Price per donut: $${foods[i].price}</p>
+              </div>
             </div>
-            <div class="col-md-6">
-              <p class="price">Per dozen: $${(foods[i].price  * 10).toFixed(2)}</p>
-          </div>
 
-          <div class="row">
-           <form>
-
-              <div class="col-lg-4">
-                <div class="qty mt-5">
-                <span class="minus bg-dark">-</span>
-                  <input type="text" id="donut-qty-${foods[i].id}" class="count" name="qty" value="3">
-                <span class="plus bg-dark">+</span>
-               </div>
-              </div>
-              <div class="col-lg-4">
-               <button class="toCart" onclick="add(${foods[i].id}, 'donut-qty-${foods[i].id}')" type="button"> Add to cart </button>
-              </div>
-                 <div class="col-lg-4">
-               <select id="donut-size-${foods[i].id}" name="size">
-                  <option>Individual</option>
-                  <option>Dozen</option>
-                </select>
-              </div>
-            </form>
-          </div>
-
-
-
-
-
-
-
+            <div class="row">
+              <form>
+                <div class="col-lg-4">
+                  <div class="qty mt-5">
+                    <span class="minus bg-dark">-</span>
+                      <input type="text" id="donut-qty-${foods[i].id}" class="count" name="qty" value="3">
+                    <span class="plus bg-dark">+</span>
+                  </div>
+                </div>
+                <div class="col-lg-4">
+                  <button class="toCart" onclick="add(${foods[i].id}, 'donut-qty-${foods[i].id}')" type="button"> Add to cart </button>
+                </div>
+              </form>
+            </div>
 
           </article>
 
 
           <article class="col-md-6">
-            <p class="menu_title">${foods[i+1].name}</p>
-            <img src=${foods[i+1].image_url}>
-            <p class="price">Per donut: $${foods[i].price}</p>
-            <p class="price">Per dozen: $${(foods[i].price  * 10).toFixed(2)}</p>
+
+            <div class="row">
+              <div class="col-md-12">
+                <img src=${foods[i+1].image_url}>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                 <p class="menu_title">${foods[i+1].name}</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <p class="price">Price per donut: $${foods[i+1].price}</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <form>
+                <div class="col-lg-4">
+                  <div class="qty mt-5">
+                    <span class="minus bg-dark">-</span>
+                      <input type="text" id="donut-qty-${foods[i+1].id}" class="count" name="qty" value="3">
+                    <span class="plus bg-dark">+</span>
+                  </div>
+                </div>
+                <div class="col-lg-4">
+                  <button class="toCart" onclick="add(${foods[i+1].id}, 'donut-qty-${foods[i+1].id}')" type="button"> Add to cart </button>
+                </div>
+              </form>
+            </div>
+
           </article>
         </div>
       `);
@@ -76,11 +90,41 @@ $(() => {
     if (i === foods.length - 1) {
       $(".container").append(`
         <div class="row">
-          <article class="col-md">
-            <p class="menu_title">${foods[i].name}</p>
-            <img src=${foods[i].image_url}>
-            <p class="price">Per donut: $${foods[i].price}</p>
-            <p class="price">Per dozen: $${(foods[i].price  * 10).toFixed(2)}</p>
+          <article class="col-md-6">
+
+            <div class="row">
+              <div class="col-md-12">
+                <img src=${foods[i].image_url}>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                 <p class="menu_title">${foods[i].name}</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <p class="price">Price per donut: $${foods[i].price}</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <form>
+                <div class="col-lg-4">
+                  <div class="qty mt-5">
+                    <span class="minus bg-dark">-</span>
+                      <input type="text" id="donut-qty-${foods[i].id}" class="count" name="qty" value="3">
+                    <span class="plus bg-dark">+</span>
+                  </div>
+                </div>
+                <div class="col-lg-4">
+                  <button class="toCart" onclick="add(${foods[i].id}, 'donut-qty-${foods[i].id}')" type="button"> Add to cart </button>
+                </div>
+              </form>
+            </div>
+
           </article>
         </div>
       `);
@@ -97,28 +141,42 @@ function add(id, qtyId) {
   let idStr = String(id)
   let qty = $('#'+qtyId).val();
 
-  console.log(idStr, qty);
+  // console.log(idStr, qty);
 
   let cookies = document.cookie;
-  let cookieArr = cookies.split(";");
+  console.log(cookies);
+  let cookieArr = cookies.split("; ");
+  // console.log(cookieArr);
   let cookieArrArr = [];
   for (let cookie of cookieArr) {
     cookieArrArr.push(cookie.split("="))
   }
+  // console.log(cookieArrArr.length);
 
-  for (let arr of cookieArrArr) {
-    if (idStr === arr[0]) {
-      let newCookie = Number(arr[1]) + Number(qty);
-      document.cookie = `${idStr}=${newCookie}`
-      console.log(newCookie);
-    } else {
+  for (let i = 0; i <= cookieArrArr.length; i++) {
+    // console.log(idStr === cookieArrArr[i][0]);
+    // console.log(cookieArrArr[i][0]);
+    if (i === cookieArrArr.length) {
       document.cookie = `${idStr}=${qty}`
+    } else if (idStr === cookieArrArr[i][0]) {
+      let newCookie = Number(cookieArrArr[i][1]) + Number(qty);
+      console.log(newCookie);
+      document.cookie = `${idStr}=${newCookie}`
+      break;
     }
   }
 
+  // for (let arr of cookieArrArr) {
+  //   if (idStr === arr[0]) {
+  //     console.log(newCookie);
+  //   } else {
+  //   }
+  // }
 
 
-  console.log(cookieArrArr);
+
+
+  // console.log(cookieArrArr);
 
   // console.log(test);
 
