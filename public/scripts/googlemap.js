b2761e147bc33557c289fcfcd8c925eda6131443
Alissa Balge <alissa.balge@gmail.com>
2018-10-19 18:52:44 +0000

    set up checkout page, has google maps api

---
+++ public/scripts/googlemap.js
@@ -0,0 +1,11 @@
+// Initialize and add the map
+function initMap() {
+    // The location of LHL
+var lighthouse = {lat: 43.644618, lng: -79.394891};
+  // The map, centered at LHL
+var map = new google.maps.Map(
+document.getElementById('map'), {zoom: 15, center: lighthouse});
+      // The marker, positioned at Uluru
+var marker = new google.maps.Marker({position: lighthouse, map: map});
+      }
+
