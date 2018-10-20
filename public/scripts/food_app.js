98c12a26e9ed08982e42d23a2c841adec5b52055
Alissa Balge <alissa.balge@gmail.com>
2018-10-20 01:49:46 +0000

    created better layout for menu page

--- public/scripts/food_app.js
+++ public/scripts/food_app.js
@@ -7,79 +7,74 @@ $(() => {
     let i;
     for (i = 0; i < foods.length - 1; i += 2) {
       //$(".container").append(`<div>${foods[i].name}</div>`);
-      $(".container").append(`
+      $("#menu").append(`
         <div class="row">
-          <article class="col-md-6">
 
+          <article class="col-md-6">
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                 <img src=${foods[i].image_url}>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                  <p class="menu_title">${foods[i].name}</p>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                 <p class="price">Price per donut: $${foods[i].price}</p>
               </div>
             </div>
 
             <div class="row">
-              <form>
-                <div class="col-lg-4">
-                  <div class="qty mt-5">
-                    <span class="minus bg-dark" onclick="minus('donut-qty-${foods[i].id}')">-</span>
-                      <input type="text" id="donut-qty-${foods[i].id}" class="count" name="qty" value="1">
-                    <span class="plus bg-dark" onclick="plus('donut-qty-${foods[i].id}')">+</span>
+                <div class="col-md-12" align="center">
+                  <div class="qty">
+                    <div class="minus bg-dark" onclick="minus('donut-qty-${foods[i].id}')">-</div>
+                      <input type="text" min="1" id="donut-qty-${foods[i].id}" class="count" name="qty" value="1">
+                    <div class="plus bg-dark" onclick="plus('donut-qty-${foods[i].id}')">+</div>
                   </div>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-md-12" align="center">
                   <button class="toCart" onclick="add(${foods[i].id}, 'donut-qty-${foods[i].id}')" type="button"> Add to cart </button>
                 </div>
-              </form>
             </div>
-
           </article>
 
 
           <article class="col-md-6">
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                 <img src=${foods[i+1].image_url}>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                  <p class="menu_title">${foods[i+1].name}</p>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                 <p class="price">Price per donut: $${foods[i+1].price}</p>
               </div>
             </div>
 
-            <div class="row">
-              <form>
-                <div class="col-lg-4">
-                  <div class="qty mt-5">
-                    <span class="minus bg-dark" onclick="minus('donut-qty-${foods[i+1].id}')">-</span>
-                      <input type="text" id="donut-qty-${foods[i+1].id}" class="count" name="qty" value="1">
-                    <span class="plus bg-dark" onclick="plus('donut-qty-${foods[i+1].id}')">+</span>
+             <div class="row">
+                <div class="col-md-12" align="center">
+                  <div class="qty">
+                    <div class="minus bg-dark" onclick="minus('donut-qty-${foods[i+1].id}')">-</div>
+                      <input type="text" min="1" id="donut-qty-${foods[i+1].id}" class="count" name="qty" value="1">
+                    <div class="plus bg-dark" onclick="plus('donut-qty-${foods[i+1].id}')">+</div>
                   </div>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-md-12" align="center">
                   <button class="toCart" onclick="add(${foods[i+1].id}, 'donut-qty-${foods[i+1].id}')" type="button"> Add to cart </button>
                 </div>
-              </form>
             </div>
 
           </article>
@@ -88,43 +83,40 @@ $(() => {
     }
     // console.log(i);
     if (i === foods.length - 1) {
-      $(".container").append(`
+      $("#menu").append(`
         <div class="row">
           <article class="col-md-6">
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                 <img src=${foods[i].image_url}>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                  <p class="menu_title">${foods[i].name}</p>
               </div>
             </div>
 
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" align="center">
                 <p class="price">Price per donut: $${foods[i].price}</p>
               </div>
             </div>
 
-            <div class="row">
-              <form>
-                <div class="col-lg-4">
-                  <div class="qty mt-5">
-                    <span class="minus bg-dark" onclick="minus('donut-qty-${foods[i].id}')">-</span>
-                    <input type="text" id="donut-qty-${foods[i].id}" class="count" name="qty" value="1">
-                    <span class="plus bg-dark" onclick="plus('donut-qty-${foods[i].id}')">+</span>
+              <div class="row">
+                <div class="col-md-12" align="center">
+                  <div class="qty">
+                    <div class="minus bg-dark" onclick="minus('donut-qty-${foods[i].id}')">-</div>
+                      <input type="text" min="1" id="donut-qty-${foods[i].id}" class="count" name="qty" value="1">
+                    <div class="plus bg-dark" onclick="plus('donut-qty-${foods[i].id}')">+</div>
                   </div>
                 </div>
-                <div class="col-lg-4">
+                <div class="col-md-12" align="center">
                   <button class="toCart" onclick="add(${foods[i].id}, 'donut-qty-${foods[i].id}')" type="button"> Add to cart </button>
                 </div>
-              </form>
             </div>
-
           </article>
         </div>
       `);
