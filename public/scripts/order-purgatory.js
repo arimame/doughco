b7c94651d37d21912b79e2b21ec4d7672c64b25d
Connor Peebles <connorpeebles93@gmail.com>
2018-10-21 20:45:04 +0000

    cleaned up the order purgatory ejs file

--- public/scripts/order-purgatory.js
+++ public/scripts/order-purgatory.js
@@ -1,8 +1,8 @@
 $(() => {
-    let eventSource = new EventSource('http://localhost:8080/check-status');
-    
-    eventSource.addEventListener('received', (e) => {
-        console.log('order received. redirecting...');
-        window.location.href = "/order-confirmed/" + clientPhone;
-    });
+  let eventSource = new EventSource('http://localhost:8080/check-status');
+
+  eventSource.addEventListener('received', (e) => {
+      console.log('order received. redirecting...');
+      window.location.href = "/order-confirmed/" + clientPhone;
+  });
 });
