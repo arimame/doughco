3814e4b00cd5ccd190e712a3fc8670d4a64df6ae
Connor Peebles <connorpeebles93@gmail.com>
2018-10-21 16:43:04 +0000

    cleaned up myaccount.ejs and all scripts thatrun in myaccount

--- public/scripts/account-details.js
+++ public/scripts/account-details.js
@@ -2,6 +2,7 @@ $(() => {
 
   let phone, email, name, card_number, card_security, card_expire;
 
+  // get account info from database to display
   $.ajax({
     method: "GET",
     url: "/api/users/" + currUser
@@ -12,26 +13,37 @@ $(() => {
       card_number = user[0].card_number;
       card_security = user[0].card_security;
       card_expire = user[0].card_expire;
-      // $('#current-email').text(`Current email: ${email}`);
-      // $('#current-phone').text(`Current phone number: ${phone || 'not set'}`);
+
+      // display email
       $('#email').attr('value', email);
+
+      // display phone number or prompt to enter
       if (phone) {$('#tel').attr('value', phone);}
       else {$('#tel').attr('placeholder', 'Please enter your number (no dashes or spaces)').addClass("form-unfilled");}
+
+      // display name or prompt to enter
       if (name) {$('#user-name').attr('value', name);}
       else {$('#user-name').attr('placeholder', 'Please enter your name (as it appears on your card)').addClass("form-unfilled");}
+
+      // display last four digits of card number or prompt to enter
       if (card_number) {
         let str_card = String(card_number);
         let display_card = "************" + str_card.substring(str_card.length - 4, str_card.length);
         $('#card-number').attr('value', display_card);
       }
       else {$('#card-number').attr('placeholder', 'Please enter your card number').addClass("form-unfilled");}
+
+      // display *** in place of cvv or prompt to enter
       if (card_security) {$('#card-cvv').attr('value', '***');}
       else {$('#card-cvv').attr('placeholder', 'Please enter your card\'s CVV').addClass("form-unfilled");}
+
+      // display expiry date or prompt to enter
       if (card_expire) {$('#card-expire').attr('value', card_expire);}
       else {$('#card-expire').attr('placeholder', 'Please enter your card\'s expiry (MMYY)').addClass("form-unfilled");}
 
     });
 
+  // update credit card info in database
   $('#credit-card').submit(function(e) {
     e.preventDefault();
     $.ajax({
@@ -45,10 +57,11 @@ $(() => {
       }
     })
     .then(function(r) {
-      window.location.href = "/myaccount"
+      window.location.href = "/myaccount";
     });
   });
 
+  // update contact info in database
   $('#contact-info').submit(function(e) {
     e.preventDefault();
     $.ajax({
@@ -59,7 +72,6 @@ $(() => {
         phone: $('#tel').val()
       }
     }).then(function(r) {
-      console.log('good good');
       window.location.href = "/myaccount"
     });
   });
